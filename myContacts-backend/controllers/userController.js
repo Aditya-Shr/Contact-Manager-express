60085711a3fd79d4313954832f7b769180a97e1d
Aditya Shrivastava <shrivastavaaditya67@gmail.com>
2024-06-22 19:34:49 +0530

    Second update to code

--- myContacts-backend/controllers/userController.js
+++ myContacts-backend/controllers/userController.js
@@ -1,6 +1,7 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 //@desc Register a user
 //@route POST /api/users/register
@@ -45,7 +46,30 @@ const registerUser = asyncHandler(async (req,res)=>{
 //@access public
 
 const loginUser = asyncHandler(async (req,res)=>{
-    res.json({message: "Login User"});
+    const{email,password} = req.body;
+    if(!email||!password){
+        res.status(400);
+        throw new Error("All fields are mandatory!");
+    }
+
+    const user = await User.findOne({email});
+    //compare password with hashpassword
+    if(user && (await bcrypt.compare(password,user.password))){
+        const accesstoken = jwt.sign({
+            user:{
+                username :user.username,
+                email: user.email,
+                id:user.id,
+            },
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {expiresIn:"1m"}
+    );
+    res.status(200).json({accesstoken});
+    }else{
+        res.status(401);
+        throw new Error("email or paassword is not valid");
+    }
 });
 
 //@desc Current user info
